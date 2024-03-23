578ef1495c2f950f0e151374fb5d7086009e8b8b
Jose Nahum <josenahumacunalopez8@gmail.com>
2024-03-22 21:16:11 -0600

    Do and do - while statements

---
+++ WhileClass.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class WhileClass {
+    public static void main(String[] args) {
+/*
+*  While loop
+*
+        int i = 0;
+        while (i < 5){
+            System.out.println(" pepito " + i);
+            i++;
+        }
+* */
+
+        // Do while
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i > 5);
+    }
+}
